f2178a22063daede4abe5cd6bb07bb9d28990c6d
jamesheck2019 <48408300+jamesheck2019@users.noreply.github.com>
2019-03-10 15:47:54 +0200

    Add files via upload

---
+++ IFolder.vb
@@ -0,0 +1,11 @@
+﻿Imports DeQma.ZohoDocsSDK.JSON
+
+Public Interface IFolder
+
+    Function RenameFolder(FolderID As String, NewFolderName As String) As Task(Of JSON_RenameFolder)
+    Function TrashFolder(FolderID As String) As Task(Of JSON_TrashFolder)
+    Function CreateNewFolder(FolderName As String, Optional DestinationFolderID As String = Nothing) As Task(Of JSON_CreateNewFolder)
+    Function MoveFolder(FolderID As String, ParentFolderID As String, DestinationFolderID As String) As Task(Of JSON_MoveFolder)
+    Function CopyFolder(FolderID As String, DestinationFolderID As String) As Task(Of JSON_CopyFolder)
+    Function DeleteFolder(FolderID As String) As Task(Of JSON_DeleteFolder)
+End Interface
---
+++ IListing.vb
@@ -0,0 +1,9 @@
+﻿Imports DeQma.ZohoDocsSDK.ZhOutilities
+
+Public Interface IListing
+    Function ListAllFiles(Optional Category As Category = Category._ALL_, Optional Offset As String = Nothing, Optional Limit As String = Nothing) As Task(Of JSON_FilesMetadata)
+    Function ListAllFolders(DestinationFolderID As String) As Task(Of JSON_FoldersMetadata)
+    Function ListSubFilesAndFolders(DestinationFolderID As String) As Task(Of JSON_FilesFoldersMetadata)
+    Function ListSubFilesAndFolders2(DestinationFolderID As String) As Task(Of JSON_FilesFoldersMetadata)
+    Function ListRootFilesFolders() As Task(Of JSON_FilesFoldersMetadata)
+End Interface
